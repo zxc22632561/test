07826bb02fe467bc66d4e61ab3def761f87279a7
zxc22632561 <zxc22632561@gmail.com>
2017-12-27 17:16:07 +0800

    asp.net

---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        String res = "";
+        int n = Convert.ToInt32(TextBox1.Text);
+
+        for(int i=1; i <= n; i++)
+        {
+            for(int j=1; j <= i; j++)
+            {
+                res += i + "*" + j + "=" + (i * j)+"\t";
+            }
+            res += "\r\n"; //跳行
+        }
+        txtOutput.Text = res;
+    }
+}
